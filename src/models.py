b59f6750a7fa2fd5d3bd570fb6859a01e7c56a19
fmatovelle <167099575+fmatovelle@users.noreply.github.com>
2024-08-01 20:17:48 +0000

    Finalizado

--- src/models.py
+++ src/models.py
@@ -9,8 +9,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-# Definir el Enum para el campo 'type' en la clase 'Media'
-class MediaType(enum.Enum):  # Definimos un Enum con los valores posibles para 'type'
+class MediaType(enum.Enum): 
     IMAGE = "image"
     VIDEO = "video"
     AUDIO = "audio"
@@ -39,10 +38,9 @@ class Post(Base):
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    type = Column(Enum(MediaType), nullable=False)  # Usando el Enum definido
+    type = Column(Enum(MediaType), nullable=False) 
     url = Column(String(250), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)  # Agregamos la relación ForeignKey
-
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)  
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
@@ -55,7 +53,7 @@ class Follower(Base):
     user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
-## Draw from SQLAlchemy base
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
